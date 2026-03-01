02bc4eb4b5b3ae9cb4561224ac5810cf6a315777
teafull <zsf0115@gmail.com>
2026-03-01 19:39:09 +0800

    feat: 添加 Element Plus 中文语言支持

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from "vue";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+// 引入 Element 的中文语言包
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import App from "./App.vue";
 import router from './router'
 
-createApp(App).use(ElementPlus).use(router).mount("#app");
+createApp(App).use(ElementPlus, {
+  locale: zhCn
+}).use(router).mount("#app");
 
